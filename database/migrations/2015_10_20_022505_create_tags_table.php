181d78c50841e4e71a8858d88ca2bb4ca35563cd
silence <csiris@126.com>
2015-10-20 13:50:13 +0800

    zd

---
+++ database/migrations/2015_10_20_022505_create_tags_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tags', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('name');
+            $table->string('niko', 100);
+            $table->time('time1');
+            $table->timestamp('timestamp');
+            $table->softDeletes();
+            $table->morphs('morphs');
+            $table->text('remark');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('tags');
+    }
+}
